61e7799770d5d25a69d8eef3ef0a4290c9615054
Melak-tefera <tmeleak@gmail.com>
2026-07-07 22:05:10 +0100

    some changes

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -10,17 +10,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My First App',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.yellow,
+        appBar: AppBar(
+          title: Center(child: Text("My App")),
+          backgroundColor: Color.fromARGB(255, 240, 7, 194),
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+
+        ),
         body: Center(
           child: Container(
-              height: 300,
-              width: 300,
-              color: const Color.fromARGB(255, 140, 108, 118)
-              
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color:Color.fromARGB(255, 240, 7, 194),
+              borderRadius: BorderRadius.circular(20),
             ),
-        ),
+            padding: EdgeInsets.all(25),
+            child: Icon(Icons.home,
+                  color: Colors.yellow,
+                  size: 64,
+            )
+
+          ),
+        )
+        
         
       ),
       );
