33be2713c5f6bce83dfcc0f7a407b35a326262ee
Melak-tefera <tmeleak@gmail.com>
2026-07-09 19:27:56 +0100

    some other changes

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/pages/first_page.dart';
-import 'package:todo_list/pages/home_page.dart';
-import 'package:todo_list/pages/profile_page.dart';
-import 'package:todo_list/pages/setting_page.dart';
+import 'package:todo_list/pages/counter_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,13 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:FirstPage() ,
-      routes: {
-        "/firstpage":(context) => FirstPage(),
-        "/homepage":(context) => HomePage(),
-        "/profilepage":(context) => ProfilePage(),
-        "/settingpage":(context) => SettingPage()
-      },
+      home:CounterPage(),
       );
   }
 }
